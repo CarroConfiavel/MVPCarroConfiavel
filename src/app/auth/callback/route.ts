cdd83326467023f2aa23bd64a3ae05f0061dc1e3
Name <contato@carroconfiavel.com>
2025-11-06 01:16:48 +0000

    auth callback

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@/supabase/server';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
+
+  if (code) {
+    const supabase = await createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // Redireciona o usuário para a página principal após a confirmação
+  return NextResponse.redirect(requestUrl.origin);
+}
